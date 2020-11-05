6f04365188bf7a696a643a1d349213302f65af53
daabraga <daabraga@gmail.com>
2020-11-04 22:04:25 -0300

    changing name

--- estoque/settings.py
+++ estoque/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #my-apps
-    'estoque.core',
-    'estoque.produto',
+    'projeto.core',
+    'ptojeto.produto',
 
 ]
 
