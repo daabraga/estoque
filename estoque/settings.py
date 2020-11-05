7a93376987985d1a21709374de3c371823fa1943
daabraga <daabraga@gmail.com>
2020-11-04 22:02:30 -0300

    Create app produto

--- estoque/settings.py
+++ estoque/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #my-apps
-    'projeto.core',
+    'estoque.core',
+    'estoque.produto',
+
 ]
 
 MIDDLEWARE = [
